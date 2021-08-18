59f5092c1b4f453a38e58c0d3b4f0649457f773d
gossamr <git@gossamr.org>
2021-08-18 17:27:50 +0700

    default to base client

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,6 @@ module.exports = {
   entry: {
     client: path.resolve(__dirname, './src/client.js'),
     'client.base': path.resolve(__dirname, './src/client.base.js'),
-    'client.flash': path.resolve(__dirname, './src/client.flash.js'),
-    'client.java': path.resolve(__dirname, './src/client.java.js'),
   },
   devtool: 'source-map',
   mode: 'production',
